4aca8ddd596b838e2464599167841b528b043ca7
hxy00 <454386264@qq.com>
2019-11-25 17:33:58 +0800

    优化前端代码

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,43 +1,76 @@
 var app = getApp();
+var Constant = require('../../utils/constant.js');
 
 Page({
-  data: {
-    userInfo: {},
-    mode: ['我的收藏', '我的订单', '我的地址', '联系客服', '关于我们']
-  },
-  onLoad: function () {
-    var that = this;
-    wx.login({
-      success: function () {
-        wx.getUserInfo({
-          success: function (res) {
-            that.setData({
-              userInfo: res.userInfo
-            })
-          }
-        })
-      }
-    });
-  },
-  onItemClick: function (event) {
-    console.log('itemClick');
-    var targetUrl = "/pages/user/location/location";
-    // if (event.currentTarget.dataset.url != null)
-    //   targetUrl = targetUrl + "?url=" + event.currentTarget.dataset.url;
-    wx.navigateTo({
-      url: targetUrl
-    });
-  },
-  collect: function(){
-    var targetUrl = "/pages/user/collect/collect";
-    wx.navigateTo({
-      url: targetUrl
-    });
-  },
-  mypublish: function () {
-    var targetUrl = "/pages/user/mypublish/mypublish";
-    wx.navigateTo({
-      url: targetUrl
-    });
-  }
+        data: {
+                userInfo: {},
+                cfd: 'xxx',
+                mdd: 'xxx',
+                mdd1: '',
+                mode: ['我的收藏', '我的订单', '我的地址', '联系客服', '关于我们']
+        },
+        onLoad: function() {
+                var that = this;
+                wx.login({
+                        success: function() {
+                                wx.getUserInfo({
+                                        success: function(res) {
+                                                that.setData({
+                                                        userInfo: res.userInfo
+                                                })
+                                        }
+                                })
+                        }
+                });
+        },
+        onItemClick: function(event) {
+                console.log('itemClick');
+                var targetUrl = "/pages/user/location/location";
+                // if (event.currentTarget.dataset.url != null)
+                //   targetUrl = targetUrl + "?url=" + event.currentTarget.dataset.url;
+                wx.navigateTo({
+                        url: targetUrl
+                });
+        },
+        collect: function() {
+                var targetUrl = "/pages/user/collect/collect";
+                wx.navigateTo({
+                        url: targetUrl
+                });
+        },
+        mypublish: function() {
+                var targetUrl = "/pages/user/mypublish/mypublish?photoUrl=" + this.data.userInfo.avatarUrl;
+                wx.navigateTo({
+                        url: targetUrl
+                });
+        },
+        testConnect: function() {
+                wx.showToast({
+                        title: '加载中',
+                        icon: 'loading'
+                });
+                wx.request({
+                        url: Constant.TEST_URL,
+                        header: {
+                                "Content-Type": "application/json"
+                        },
+                        success: function(res) {
+                                console.log('test ---------->' + res)
+                                wx.showToast({
+                                        icon: 'success',
+                                        title: res.data.retMsg,
+                                        duration: 3000
+                                })
+                                // wx.hideToast();
+                        },
+                        fail: function(res) {
+                                console.log(res)
+                                wx.showToast({
+                                        icon: 'none',
+                                        title: res.errMsg,
+                                        duration: 3000
+                                })
+                        }
+                });
+        }
 })
